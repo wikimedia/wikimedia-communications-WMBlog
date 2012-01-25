573aaaf38d398905cbfbad52c5e717e13501c198
Guillaume Paumier <guillom.pom@gmail.com>
2012-01-25 15:58:30 +0100

    minor code formatting

--- WMBlog.php
+++ WMBlog.php
@@ -24,12 +24,16 @@ License: GPLv3
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
-/* TODO: Create a plugin options page in the admin interface
+
+/* =========================================================
+   TODO: Create a plugin options page in the admin interface
    with checkboxes for each option, so that any admin can 
    enable / disable a specific feature easily, without the 
    need to do it here */
 
-/* Allow contributors to upload and insert files into their posts.
+
+/* =========================================================
+   Allow contributors to upload and insert files into their posts.
    This setting is written to the database. */
 
 function WMBlog_allow_contributor_uploads() {
@@ -37,7 +41,8 @@ function WMBlog_allow_contributor_uploads() {
 	$contributor->add_cap('upload_files');
 }
 
-/* Do stuff when the plugin is activated. This includes calling the other
+/* =========================================================
+   Do stuff when the plugin is activated. This includes calling the other
    plugin functions that actually do stuff. */
 
 register_activation_hook( __FILE__, 'WMBlog_setup' );
@@ -46,7 +51,9 @@ function WMBlog_setup() {
 	WMBlog_allow_contributor_uploads();
 }
 
-/* Do stuff when the plugin is deactivated. This includes removing some settings from
+
+/* =========================================================
+   Do stuff when the plugin is deactivated. This includes removing some settings from
    the database that have been written by the plugin */
 
 register_deactivation_hook( __FILE__, 'WMBlog_cleanup' );
